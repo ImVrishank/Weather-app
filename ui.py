ac8c1df06114e1b2477b4197630b46f6349d9561
Vrishank <106276129+ImVrishank@users.noreply.github.com>
2024-12-02 23:27:23 +0530

    bettered code

--- ui.py
+++ ui.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
 from app import main as get_weather
+from app import api_key
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    return render_template('index.html')
+    data = None
+    if request.method == 'POST':
+        city = request.form['cityName']
+        data = get_weather(city, api_key)
+    return render_template('index.html', data = data)
 
 
 if __name__ == "__main__":
