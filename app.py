66196ee3f28943bf007d7d1070bb6e8bd4268873
Vrishank <106276129+ImVrishank@users.noreply.github.com>
2024-12-02 21:07:44 +0530

    added code

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+
+#get the api key from openweathermap.org 
+api_key = "766443a8a1db73992dc192eabba6b5d8"
+
+user_input = input("enter city: ")
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºF")
+
+
+
+
